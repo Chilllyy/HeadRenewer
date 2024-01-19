ab462da47c65b9deaa4b9a731a9f7c6d122baa61
colby <colby@techrisdesign.com>
2024-01-19 13:49:17 -0700

    Added command

--- src/main/java/me/chillywilly/HeadRenewer.java
+++ src/main/java/me/chillywilly/HeadRenewer.java
@@ -1,15 +1,39 @@
 package me.chillywilly;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class HeadRenewer extends JavaPlugin {
+import net.md_5.bungee.api.ChatColor;
+
+public class HeadRenewer extends JavaPlugin implements CommandExecutor {
+    
     @Override
     public void onEnable() {
-        //TODO Enable Plugin
+        //TODO Enable Function
+        getLogger().info("Plugin is enabled!");
+        this.getCommand("renewhead").setExecutor(this);
     }
 
     @Override
     public void onDisable() {
-        //TODO Disable Plugin
+        getLogger().info("Plugin is disabled!");
+    }
+
+    public String color(String msg) {
+        return ChatColor.translateAlternateColorCodes('&', msg);
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            player.sendMessage(color("&4test"));
+        } else {
+            sender.sendMessage("This command only applies to players");
+        }
+        return true;
     }
 }
