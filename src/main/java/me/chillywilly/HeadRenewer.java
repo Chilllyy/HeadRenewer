d980ff65e087d1be00d60b372e37b4578be20da9
colby <colby@techrisdesign.com>
2024-01-19 12:34:11 -0700

    Init maven project

---
+++ src/main/java/me/chillywilly/HeadRenewer.java
@@ -0,0 +1,15 @@
+package me.chillywilly;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class HeadRenewer extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        //TODO Enable Plugin
+    }
+
+    @Override
+    public void onDisable() {
+        //TODO Disable Plugin
+    }
+}
